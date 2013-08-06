e04cb8290b462ba3820edab168dc1e56595f3be1
Matthew Woehlke <matthew.woehlke@kitware.com>
2013-08-06 17:09:35 -0400

    Fix adding functions to instantiations
    Add the little tiny missing piece of code that broke adding new methods
    to a template instantiation type. Add unit test for this.
    
    Change-Id: I4e03805d83669af3b036451845394ca7a5a95276

--- ApiExtractor/abstractmetabuilder.cpp
+++ ApiExtractor/abstractmetabuilder.cpp
@@ -1416,6 +1416,8 @@ AbstractMetaClass* AbstractMetaBuilder::createInstantiationMetaClass(ComplexType
     if (!entry->include().isValid())
         setInclude(entry, entry->include().name());
 
+    fillAddedFunctions(metaClass);
+
     return metaClass;
 }
 
