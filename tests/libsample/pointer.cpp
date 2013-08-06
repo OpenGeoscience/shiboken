a0eaef6b7766d9f4976e1880cfb4abbadb800960
Matthew Woehlke <matthew.woehlke@kitware.com>
2013-08-06 15:51:01 -0400

    Start implementing template class support
    Add 'type-template' tag (and 'arg' child tag) to declare a definition of
    a template struct/class. Modify 'object-type' and 'value-type' to allow
    creating these as template instantiations referencing a 'type-template',
    as an alternative to the usual non-template support.
    
    There is also a 'redirect' attribute on 'arg' that will be used to
    create "redirection" methods on the wrapped class to allow calling
    methods of a template argument class as if they belonged to the
    instantiation, which is sort of like having an operator->().
    
    The eventual goal is to support wrapping of e.g. QSharedPointer.
    
    This is sufficient to parse a typesystem XML (updates to 'sample'
    included) using the new tags, but not to do anything useful yet, as
    neither type resolution is working, nor do we generate code for the
    instantiations.
    
    Change-Id: I37960727c4dd69c39e84a414ae08752e3fd0dbf3

---
+++ tests/libsample/pointer.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2013 Kitware, Inc.
+ *
+ * This file is part of the Shiboken Python Binding Generator project.
+ *
+ * Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
+ *
+ * Contact: PySide team <contact@pyside.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "pointer.h"
+
+static int nextId = 0;
+static int counter = 0;
+
+SimpleObject::SimpleObject(int id) : m_id(id)
+{
+    ++counter;
+}
+
+SimpleObject::~SimpleObject()
+{
+    --counter;
+}
+
+Pointer<SimpleObject> SimpleObject::create()
+{
+    return Pointer<SimpleObject>(new SimpleObject(++nextId));
+}
+
+int SimpleObject::count()
+{
+    return counter;
+}
