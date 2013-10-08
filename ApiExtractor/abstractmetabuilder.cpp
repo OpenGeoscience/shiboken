9048ceb36ed83a2ec38421b222883734d949b6bd
Ben Boeckel <ben.boeckel@kitware.com>
2013-10-08 14:00:54 -0400

    Sort classes with injected dependencies

--- ApiExtractor/abstractmetabuilder.cpp
+++ ApiExtractor/abstractmetabuilder.cpp
@@ -3179,7 +3179,8 @@ AbstractMetaClassList AbstractMetaBuilder::classesTopologicalSorted(const Abstra
     QRegExp regex1("\\(.*\\)");
     QRegExp regex2("::.*");
     foreach (AbstractMetaClass* clazz, classList) {
-        if (clazz->isInterface() || !clazz->typeEntry()->generateCode())
+        if (!clazz->hasInjectedDependencies() &&
+            (clazz->isInterface() || !clazz->typeEntry()->generateCode()))
             continue;
 
         if (clazz->enclosingClass() && map.contains(clazz->enclosingClass()->qualifiedCppName()))
