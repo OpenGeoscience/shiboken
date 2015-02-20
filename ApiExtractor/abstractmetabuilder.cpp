117ced106abfa9bd5eb052f1e5d82af4ee7985f1
Matthew Woehlke <matthew.woehlke@kitware.com>
2015-02-20 17:19:25 -0500

    Don't ignore classes in topology
    When building the class topology, don't skip classes, even if we are not
    going to generate code for them. This is necessary to get the topology
    order correct in a case such as C derived from B derived from A, where B
    is not generated, but initializing C depends on A being initialized
    first. Without this change, there is no guaranteed ordering between A
    and C in such a case.
    
    (In particular, this comes up in the Photon test; Photon::ValueIdentity
    derives from Photon::TemplateBase, which derives from Photon::Base.
    However, this was not being reflected in the topology, and as a result,
    it was just luck that the initialization order was correct anyway.)

--- ApiExtractor/abstractmetabuilder.cpp
+++ ApiExtractor/abstractmetabuilder.cpp
@@ -3212,10 +3212,6 @@ AbstractMetaClassList AbstractMetaBuilder::classesTopologicalSorted(const Abstra
     QRegExp regex1("\\(.*\\)");
     QRegExp regex2("::.*");
     foreach (AbstractMetaClass* clazz, classList) {
-        if (!clazz->hasInjectedDependencies() &&
-            (clazz->isInterface() || !clazz->typeEntry()->generateCode()))
-            continue;
-
         if (clazz->enclosingClass() && map.contains(clazz->enclosingClass()->qualifiedCppName()))
             graph.addEdge(map[clazz->enclosingClass()->qualifiedCppName()], map[clazz->qualifiedCppName()]);
 
